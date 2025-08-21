0a11e20f0828c93cd061f57ccdd84ccdc924b7e0
Scott Cowe <scott.t.cowe@gmail.com>
2025-08-21 22:48:48 +0100

    nix setup

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+
+  buildInputs = with pkgs; [
+    rust-analyzer
+    rustfmt
+    clippy
+  ];
+}
